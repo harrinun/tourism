ad669ae596e83c90b0c5e60b6fc850e067fadd02
harrinun <harrinun@gmail.com>
2017-04-27 11:31:21 +0000

    slider edits

--- resources/views/layouts/site/hero.blade.php
+++ resources/views/layouts/site/hero.blade.php
@@ -8,33 +8,46 @@
                     Last minute deal
                 </div>
 
-                <div class="tp-caption sfb fadeout slider-caption slider-caption-1" data-x="center" data-y="280" data-speed="700" data-easing="easeOutBack"  data-start="2000">Top discount Paris Hotels</div>
+                <div class="tp-caption sfb fadeout slider-caption slider-caption-1" data-x="center" data-y="280" data-speed="700" data-easing="easeOutBack"  data-start="2000">Top discount Accra Hotels</div>
 
                 <a href="#" class="tp-caption sfb fadeout awe-btn awe-btn-style3 awe-btn-slider" data-x="center" data-y="380" data-easing="easeOutBack" data-speed="700" data-start="2200">Book now</a>
             </li>
 
-            <li data-slotamount="7" data-masterspeed="500" data-title="Slide title 2">
-                <img src="images/bg/2.jpg" data-bgposition="left center" data-duration="14000" data-bgpositionend="right center" alt="">
+            <li data-slotamount="7" data-masterspeed="500" data-title="Slide title 1">
+                <img src="images/bg/4.jpg" data-bgposition="left center" data-duration="14000" data-bgpositionend="right center" alt="">
+
+                <div class="tp-caption sfb fadeout slider-caption-sub slider-caption-sub-1" data-x="500" data-y="230" data-speed="700" data-start="1500" data-easing="easeOutBack">
+                    Special Weekend Offers
+                </div>
+
+                <div class="tp-caption sfb fadeout slider-caption slider-caption-1" data-x="center" data-y="280" data-speed="700" data-easing="easeOutBack"  data-start="2000">Super Hot Deals</div>
+
+                <a href="#" class="tp-caption sfb fadeout awe-btn awe-btn-style3 awe-btn-slider" data-x="center" data-y="380" data-easing="easeOutBack" data-speed="700" data-start="2200">Book now</a>
+            </li>
+
+
+            <li data-slotamount="7" data-masterspeed="500" data-title="Slide title 3">
+                <img src="images/bg/3.jpg" data-bgposition="left center" data-duration="14000" data-bgpositionend="right center" alt="">
 
                 <div class="tp-caption  sft fadeout slider-caption-sub slider-caption-sub-2" data-x="center" data-y="220" data-speed="700" data-start="1500" data-easing="easeOutBack">
-                    Check out the top weekly destination
+                    Best Deals Ever
                 </div>
 
                 <div class="tp-caption sft fadeout slider-caption slider-caption-2" data-x="center" data-y="260" data-speed="700" data-easing="easeOutBack"  data-start="2000">
                     Travel with us
                 </div>
 
-                <a href="#" class="tp-caption sft fadeout awe-btn awe-btn-style3 awe-btn-slider" data-x="center" data-y="370" data-easing="easeOutBack" data-speed="700" data-start="2200">Make Reservation</a>
+                <a href="#" class="tp-caption sft fadeout awe-btn awe-btn-style3 awe-btn-slider" data-x="center" data-y="370" data-easing="easeOutBack" data-speed="700" data-start="2200">Book Trip</a>
             </li>
 
-            <li data-slotamount="7" data-masterspeed="500" data-title="Slide title 3">
-                <img src="images/bg/3.jpg" data-bgposition="left center" data-duration="14000" data-bgpositionend="right center" alt="">
+            <li data-slotamount="7" data-masterspeed="500" data-title="Slide title 4">
+                <img src="images/bg/5.jpg" data-bgposition="left center" data-duration="14000" data-bgpositionend="right center" alt="">
 
                 <div class="tp-caption lfl fadeout slider-caption slider-caption-3" data-x="center" data-y="260" data-speed="700" data-easing="easeOutBack"  data-start="1500">
-                    gh360
+                           Explore Ghana
                 </div>
 
-                <div href="#" class="tp-caption lfr fadeout slider-caption-sub slider-caption-sub-3" data-x="center" data-y="365" data-easing="easeOutBack" data-speed="700" data-start="2000">Take you to every corner of the Ghana</div>
+                <div href="#" class="tp-caption lfr fadeout slider-caption-sub slider-caption-sub-3" data-x="center" data-y="465" data-easing="easeOutBack" data-speed="700" data-start="2000">Taking you to every corner of the Motherland</div>
             </li>
 
         </ul>
